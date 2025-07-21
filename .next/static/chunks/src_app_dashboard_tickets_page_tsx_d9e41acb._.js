771d02e1561748bc7516d9cdabe75e9a6405e5a1
michaelfrymier <80269717+michaelfrymier@users.noreply.github.com>
2025-07-21 11:15:23 -0400

    Add .next/static/chunks/src_app_dashboard_tickets_page_tsx_d9e41acb._.js 1753110923522

---
+++ .next/static/chunks/src_app_dashboard_tickets_page_tsx_d9e41acb._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_fd2e94ba._.js",
+  "static/chunks/node_modules_a314f1f1._.js"
+],
+    source: "dynamic"
+});
