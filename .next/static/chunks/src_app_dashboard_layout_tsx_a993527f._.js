8f015ef145a7aa485e7826bacc26263e10b8c6b5
michaelfrymier <80269717+michaelfrymier@users.noreply.github.com>
2025-07-21 11:15:35 -0400

    Add .next/static/chunks/src_app_dashboard_layout_tsx_a993527f._.js 1753110934935

---
+++ .next/static/chunks/src_app_dashboard_layout_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_afdebf2c._.js",
+  "static/chunks/src_1656c42c._.js"
+],
+    source: "dynamic"
+});
